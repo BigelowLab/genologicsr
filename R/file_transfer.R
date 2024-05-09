51f09f8aa5875d55cb3a85ec41f2370851229927
Ben Tupper <btupper@bigelow.org>
2024-05-09 16:23:57 -0400

    reorganize file_transfer

--- R/file_transfer.R
+++ R/file_transfer.R
@@ -97,6 +97,112 @@ remote_directory_exists <- function(uri,
 }
 
 
+
+
+
+################################################################################
+#' Test if scp is installed on the host platform
+#' Perhaps silly but symmetric with others.
+#'
+#' @family file_transfer
+#' @export
+#' @return logical, TRUE if scp is present
+has_scp <- function() {
+   basename(Sys.which("scp")) == "scp"
+} 
+
+#' Download a file using scp
+#'
+#' This assumes that SSH public-key authentication is set up.
+#'
+#' @family file_transfer
+#' @export
+#' @param url the url of the file
+#' @param dest character destination filename, by default the basename of the URL
+#' @param username the username (required)
+#' @param password the password (required)
+#' @param verbose logical, if TRUE then echo the command string
+#' @param extra character extra params for scp, currently "-q"
+#' @return integer with 0 for success
+scp_download <- function(url, dest, username = 'foo', password = 'bar',
+   verbose = FALSE, extra = '-q'){
+   
+   #https://kb.iu.edu/d/agye
+   #scp [options] username1@source_host:directory1/filename1 username2@destination_host:directory2/filename2
+   stopifnot(has_scp())
+   stopifnot(!missing(url))
+   if (missing(dest)) dest <- file.path(getwd(),basename(url[1]))
+   #stopifnot(username != 'foo')
+   #stopifnot(password != 'bar')  
+   
+   p <- httr::parse_url(url)
+   
+   CMD <- paste("scp",
+      extra,
+      paste0(username, '@', p$hostname, ':/', p$path),
+      shQuote(dest[1]))
+      
+   if (verbose) cat("[scp_download]", CMD, "\n")  
+    
+   system(CMD)
+}
+
+
+#' Upload a file using scp.  
+#'
+#' This assumes that SSH public-key authentication is set up.
+#' 
+#' @family file_transfer
+#' @export
+#' @param filename the fully qualified filename of file to upload
+#' @param url the destination url
+#' @param username the username (required)
+#' @param password the password (required)
+#' @param verbose logical, if TRUE then echo the command string
+#' @return integer 0 for success
+scp_upload <- function(filename, url, username = "foo", password = "bar",
+    verbose = FALSE){
+
+   stopifnot(has_scp())
+   stopifnot(!missing(filename) && !missing(url))
+   stopifnot(file.exists(filename))
+   #stopifnot(username != 'foo')
+   #stopifnot(password != 'bar')
+   
+   if (verbose) cat("[scp_upload]", url, "\n")
+   
+   okdir = make_remote_directory(dirname(url),
+                                 username = username, password = password,
+                                 use = 'scp', verbose = verbose)
+   if (okdir != 0) {
+      cat("[scp_upload] unable to create destination path:", dirname(url), "\n")
+      return(okdir)
+   }
+   
+   p <- httr::parse_url(url)
+   
+   # MKDIR <- paste('ssh',
+   #    paste0(username,'@',p$server), 
+   #    shQuote(paste('mkdir -p', paste0('/',dirname(p$path)))))
+   # ok <- system(MKDIR)
+   # if (ok != 0) {
+   #    cat("unable to create destination path:", p$path, "\n")
+   #    return(ok)
+   # }
+   
+   CMD <- paste("scp",
+      shQuote(filename[1]),
+      paste0(username, '@', p$hostname, ':/', p$path))
+      
+   r = system(CMD)
+   if (verbose) {
+     cat("[scp_upload]", CMD, "\n")  
+     cat(sprintf("[scp_upload] scp_upload %s successful", if (r == 0) "was" else "was not"), "\n")
+   }
+   return(r)
+}
+
+
 #' Download files using system cp
 #'
 #' This assumes permissions have been set up
@@ -120,7 +226,7 @@ cp_download <- function(url, dest,
 
 
 
-
+##################################################################
 #' Test if rsync is installed on the host platform
 #' Perhaps silly but symmetric with others.
 #'
@@ -228,109 +334,6 @@ rsync_upload <- function(filename, url, username = "foo", password = "bar", extr
       shQuote(paste('mv -f', from, to)) )
    if (verbose) cat(CMD, "\n")   
    system(CMD)
-}
-
-
-################################################################################
-#' Test if scp is installed on the host platform
-#' Perhaps silly but symmetric with others.
-#'
-#' @family file_transfer
-#' @export
-#' @return logical, TRUE if scp is present
-has_scp <- function() {
-   basename(Sys.which("scp")) == "scp"
-} 
-
-#' Download a file using scp
-#'
-#' This assumes that SSH public-key authentication is set up.
-#'
-#' @family file_transfer
-#' @export
-#' @param url the url of the file
-#' @param dest character destination filename, by default the basename of the URL
-#' @param username the username (required)
-#' @param password the password (required)
-#' @param verbose logical, if TRUE then echo the command string
-#' @param extra character extra params for scp, currently "-q"
-#' @return integer with 0 for success
-scp_download <- function(url, dest, username = 'foo', password = 'bar',
-   verbose = FALSE, extra = '-q'){
-   
-   #https://kb.iu.edu/d/agye
-   #scp [options] username1@source_host:directory1/filename1 username2@destination_host:directory2/filename2
-   stopifnot(has_scp())
-   stopifnot(!missing(url))
-   if (missing(dest)) dest <- file.path(getwd(),basename(url[1]))
-   #stopifnot(username != 'foo')
-   #stopifnot(password != 'bar')  
-   
-   p <- httr::parse_url(url)
-   
-   CMD <- paste("scp",
-      extra,
-      paste0(username, '@', p$hostname, ':/', p$path),
-      shQuote(dest[1]))
-      
-   if (verbose) cat("[scp_download]", CMD, "\n")  
-    
-   system(CMD)
-}
-
-
-#' Upload a file using scp.  
-#'
-#' This assumes that SSH public-key authentication is set up.
-#' 
-#' @family file_transfer
-#' @export
-#' @param filename the fully qualified filename of file to upload
-#' @param url the destination url
-#' @param username the username (required)
-#' @param password the password (required)
-#' @param verbose logical, if TRUE then echo the command string
-#' @return integer 0 for success
-scp_upload <- function(filename, url, username = "foo", password = "bar",
-    verbose = FALSE){
-
-   stopifnot(has_scp())
-   stopifnot(!missing(filename) && !missing(url))
-   stopifnot(file.exists(filename))
-   #stopifnot(username != 'foo')
-   #stopifnot(password != 'bar')
-   
-   if (verbose) cat("[scp_upload]", url, "\n")
-   
-   okdir = make_remote_directory(dirname(url),
-                                 username = username, password = password,
-                                 use = 'scp', verbose = verbose)
-   if (okdir != 0) {
-      cat("[scp_upload] unable to create destination path:", dirname(url), "\n")
-      return(okdir)
-   }
-   
-   p <- httr::parse_url(url)
-   
-   # MKDIR <- paste('ssh',
-   #    paste0(username,'@',p$server), 
-   #    shQuote(paste('mkdir -p', paste0('/',dirname(p$path)))))
-   # ok <- system(MKDIR)
-   # if (ok != 0) {
-   #    cat("unable to create destination path:", p$path, "\n")
-   #    return(ok)
-   # }
-   
-   CMD <- paste("scp",
-      shQuote(filename[1]),
-      paste0(username, '@', p$hostname, ':/', p$path))
-      
-   r = system(CMD)
-   if (verbose) {
-     cat(CMD, "\n")  
-     cat(sprintf("[scp_upload] scp_upload %s successful", if (r == 0) "was" else "was not"), "\n")
-   }
-   return(r)
 }
 
 
